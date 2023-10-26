1d5a272a20f62237bb63a7ca733ca548309f508d
mau <9014816@student.zadkine.nl>
2023-10-26 14:09:53 +0200

    Revert "inlog en aanmelden"
    This reverts commit d6fecb8eb9c57f077affaa5141421fc4b5f33fed.

--- routes/web.php
+++ routes/web.php
@@ -22,15 +22,29 @@ Route::get('/', function () {
 
 //if you wanna make a db query put it in the route like this
 Route::get('/test', function (){ 
+<<<<<<< HEAD
 /*
+=======
+>>>>>>> parent of d6fecb8 (inlog en aanmelden)
     $users = DB::table('tests')
             ->select('subject', 'email as user_email')
             ->get();
             
             dd($users);
+<<<<<<< HEAD
 */
 return view('test'); 
 });
 
 
+=======
+    return view('test');
+});
+
+//routes/web.php
+
+use App\Http\Controllers\BookingController;
+
+Route::get('/active-bookings', [BookingController::class, 'showActiveBookings'])->name('bookings.active');
+>>>>>>> parent of d6fecb8 (inlog en aanmelden)
 
