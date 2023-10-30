a6c76cdfc9f8ce8ca503435ef037748baede9a49
Jasper Kroon <jasper@crowndesign.nl>
2023-10-30 10:16:41 +0100

    model breaks aangemaakt en database breaks aangemaakt

---
+++ database/migrations/2023_10_30_090617_create_breaks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('breaks', function (Blueprint $table) {
+            $table->id();
+            $table->string('naam')->nullable();
+            $table->string('adres')->nullable();
+            $table->integer('cooördinaten')->nullable();
+            $table->string('voorzieningen')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('breaks');
+    }
+};
