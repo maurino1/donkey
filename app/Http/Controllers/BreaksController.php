d8444ed306b17f90a3236c82ad5381e319eecbb8
Jasper Kroon <jasper@crowndesign.nl>
2023-10-30 11:49:10 +0100

    begin gemaakt aan crud breaks

--- app/Http/Controllers/BreaksController.php
+++ app/Http/Controllers/BreaksController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 
 class BreaksController extends Controller
 {
-    //
+    public function index(){
+        $breaks = "breaks list form in BreaksController";
+        return view('breaks.index', ['breaks' => $breaks]);
+    }
 }
