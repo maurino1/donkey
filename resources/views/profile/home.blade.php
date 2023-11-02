e2ecc93b6d303e8a72c1b343996be60fcb7b497c
Jasper Kroon <jasper@crowndesign.nl>
2023-11-02 10:31:35 +0100

    adres aan create toegevoegd. style toegepast  plaatje.

---
+++ resources/views/profile/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+</head>
+<body>
+    <h1>Welkom op de Homepage</h1>
+    <ul>
+        <li><a href="index.blade.php"> breaks</a></li>
+        <li><a href="profile"></a></li>
+        <li><a href=""></a></li>
+    </ul>
+</body>
+</html>
