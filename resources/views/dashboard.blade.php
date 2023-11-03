794e7214969c121f24200efcc09e1d663ee9148a
Jasper Kroon <jasper@crowndesign.nl>
2023-11-03 23:16:03 +0100

    back knop

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,6 +30,7 @@
                         <li><a href="{{ asset('booking') }}">Actieve Bookings</a></li>
                         <li><a href="{{ asset('breaks') }}">Breaks for sight seeing</a></li>
                         <li><a href="{{ asset('restingspot/create') }}">restingspot</a></li>
+                        <li><a href="{{ asset('/') }}">Back</a></li>
                     </ul>
                 </div>
             </div>
